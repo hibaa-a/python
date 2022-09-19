b057a489424f3f508c2baa28ecb342d42a3422bd
Your Name <you@example.com>
2022-09-19 22:42:28 +0100

    adding changes and corrections

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,53 +1,59 @@
-
+    # importing the modules (operating system library and csv file)
 import os
 import csv
 
-
+    # set the path to the csv file
 budget_data = r"C:\Users\haldu\UBHM-VIRT-DATA-PT-08-2022-U-LOLC\02-Homework\03-Python\Instructions\PyBank\Resources\budget_data.csv"
 
+    # defining variables
+total_months = 0
+total = 0
+old = 0
 
+    # creating lists to store the data
 profit_loss = []
 dates = []
 
+    # reading the data and spliiting by commas
 csvfile = open(budget_data, 'r')
 csvreader = csv.reader(csvfile, delimiter=',')
 
+    # reading the header
 header = next(csvreader)
 
 for row in csvreader:
-    profit_loss.append(int(row[1]))
     dates.append(row[0])
+    profit_loss.append((int(row[1])) - old)
 
-total_months= len(dates)
+    total_months = total_months + 1
+    total = total + int(row[1])
+    old = int(row[1])
 
+del profit_loss[0]
 
-total_change = [a - b for b, a in zip(profit_loss[:-1], profit_loss[1:])]
+    #finding the average change
+total = sum(profit_loss)
+averagechange = round((total / (total_months - 1)), 2)
 
-def straight_average(n_list):
-    total = 0
-    length = len(n_list)
-
-    for n in n_list:
-        total += float(n)
-
-    return (total/length)
-
-netamount = sum(profit_loss)
-total_months = len(dates)
-
+    #finding the min/max profit change
 max_val = max(profit_loss)
 min_val = min(profit_loss)
-listminmax = list(zip(profit_loss, dates))
-max_change = max(profit_loss)
-max_date = max(dates)
-min_change = min(profit_loss)
-min_date = min(dates)
 
-results =(f" Total Months: {total_months} \n Total Revenue: ${netamount} \n Average Revenue Change: ${straight_average(total_change)} \n Greatest Increase in Profits: {max(listminmax)[1]}: {max(listminmax)[0]} \n Greatest Decrease in Profits: {min(listminmax)[1]}: {min(listminmax)[0]}")
+indexmax = profit_loss.index(max_val) +1
+indexmin = profit_loss.index(min_val) +1
 
+    #finding the min/max dates
+maxdate = dates[indexmax]
+mindate = dates[indexmin]
+
+    #printing
 print("Financial Analysis")
 print("-----------------------")
-print(results)
+print(f'Total Months: {total_months}')
+print(f'Total: ${total}')
+print(f'Average Change: ${averagechange}')
+print(f'Greatest Increase in Profits: {maxdate} (${max_val})')
+print(f'Greatest Decrease in Profits: {mindate} (${min_val})')
 
 
 csvfile.close()
